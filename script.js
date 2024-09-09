68d53455033f58befd00e8688a80a03a5ab71e19
TommyTommy10 <tommaso.grade@gmail.com>
2024-09-09 14:24:44 +0200

    Forzata nuova build

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function countdown() {
-    const nextBirthday = new Date(new Date().getFullYear() + 1, 8, 9); // Set the date to next year's birthday
+    const nextBirthday = new Date(new Date().getFullYear() + 30, 9, 2025); // Set the date to next year's birthday
     const now = new Date();
     const diff = nextBirthday - now;
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
