ad4799ee9de92a52fdda675aac32d06390a353fd
TommyTommy10 <tommaso.grade@gmail.com>
2024-09-11 20:54:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -53,4 +53,51 @@ function startConfetti() {
             y: Math.random() * canvas.height - canvas.height,
             r: Math.random() * 6 + 2,
             d: Math.random() * confettiCount,
-            color: `h
+            color: `hsl(${Math.random() * 360}, 100%, 50%)`,
+            tilt: Math.random() * 10 - 10,
+            tiltAngleIncremental: Math.random() * 0.07 + 0.05,
+            tiltAngle: 0
+        });
+    }
+
+    function drawConfetti() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        confetti.forEach((c, i) => {
+            c.tiltAngle += c.tiltAngleIncremental;
+            c.y += (Math.cos(c.d) + 1 + c.r / 2) / 2;
+            c.x += Math.sin(c.d);
+            c.tilt = Math.sin(c.tiltAngle - i / 3) * 15;
+
+            if (c.y > canvas.height) {
+                confetti[i] = {
+                    x: Math.random() * canvas.width,
+                    y: -10,
+                    r: c.r,
+                    d: c.d,
+                    color: c.color,
+                    tilt: c.tilt,
+                    tiltAngleIncremental: c.tiltAngleIncremental,
+                    tiltAngle: c.tiltAngle
+                };
+            }
+
+            ctx.beginPath();
+            ctx.lineWidth = c.r / 2;
+            ctx.strokeStyle = c.color;
+            ctx.moveTo(c.x + c.tilt + c.r, c.y);
+            ctx.lineTo(c.x + c.tilt, c.y + c.tilt + c.r);
+            ctx.stroke();
+        });
+    }
+
+    function updateConfetti() {
+        drawConfetti();
+        requestAnimationFrame(updateConfetti);
+    }
+
+    updateConfetti();
+}
+
+if (new Date().getMonth() === 8 && new Date().getDate() === 30) {
+    startConfetti();
+}
